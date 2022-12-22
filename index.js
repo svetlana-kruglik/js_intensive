a6a58d3b6764affefca66074ade1051f16e03a3f
svetlana-kruglik <kruglik.svetlana.post@gmail.com>
2022-12-22 10:54:54 +0100

    branch calculation is created and added function task02

--- index.js
+++ index.js
@@ -16,3 +16,33 @@ function task01 () {
 }
 
 task01();
+
+//task02
+function task2 () {
+	let arr = [];
+	let tempVal = 2;
+
+	while (tempVal) {
+		let a =  prompt('Ведите число?', '');
+
+		if (a != null && a !== '' && !isNaN(parseInt(a))) {
+			arr.push(a);
+			--tempVal;
+		} else {
+			console.log('Некорректный ввод!');
+			tempVal = false;
+			break;
+		}
+	}
+
+	if (arr.length === 2) {
+		let [item1, item2] = arr;
+
+		let res1 = +item1 + +item2;
+		let res2 = item1 / item2;
+
+		return `Ответ: ${res1}, ${res2}`
+	}
+}
+
+console.log(task2());
